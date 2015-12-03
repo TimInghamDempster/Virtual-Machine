f7273155211e1fe17fc10d95feb83467fc830252
Tim Ingham-Dempster <tim_ingham_dempster@hotmail.co.uk>
2015-12-03 23:15:51 +0000

    [TID] Add a block device class.

---
+++ BlockDevice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virutal_Machine
+{
+	class BlockDevice
+	{
+		InterconnectTerminal m_SystemInterconnect;
+		int m_size;
+		int m_address;
+
+		public BlockDevice(int address, InterconnectTerminal systemInterconnect, string backingFilename, int size)
+		{
+		}
+
+		public void Tick()
+		{
+		}
+	}
+}
